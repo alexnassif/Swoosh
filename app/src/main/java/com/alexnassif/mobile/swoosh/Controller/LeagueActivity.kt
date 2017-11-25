8c4882102303c805a7e0fbbca44849e2dad97b0d
Alex <anassif2@calstatela.edu>
2017-11-24 22:22:22 -0800

    lesson 8

--- app/src/main/java/com/alexnassif/mobile/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/alexnassif/mobile/swoosh/Controller/LeagueActivity.kt
@@ -3,6 +3,7 @@ package com.alexnassif.mobile.swoosh.Controller
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.alexnassif.mobile.swoosh.Model.Player
@@ -12,11 +13,24 @@ import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
 
-    var selected = ""
+
     var player = Player("", "")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun leagueNextClicked(view: View){
--- app/src/main/java/com/alexnassif/mobile/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/alexnassif/mobile/swoosh/Controller/SkillActivity.kt
@@ -19,7 +19,18 @@ class SkillActivity : AppCompatActivity() {
 
         player = intent.getParcelableExtra(EXTRA_PLAYER)
     }
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
 
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
+    }
     fun onBeginnerClick(view: View){
         ballerSkillBtn.isChecked = false
         player.skill = "beginner"
