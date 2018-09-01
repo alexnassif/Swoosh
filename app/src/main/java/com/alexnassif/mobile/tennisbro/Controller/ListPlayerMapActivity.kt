0a716d0e4f20a86ad679348695dc9b413861455d
Alex Nassif <alexnassif@Alexs-MBP.home>
2018-08-31 23:38:21 -0700

    toolbar

--- app/src/main/java/com/alexnassif/mobile/tennisbro/Controller/ListPlayerMapActivity.kt
+++ app/src/main/java/com/alexnassif/mobile/tennisbro/Controller/ListPlayerMapActivity.kt
@@ -2,29 +2,33 @@ package com.alexnassif.mobile.tennisbro.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils.replace
+import com.alexnassif.mobile.tennisbro.PlayerListFragment
 import com.alexnassif.mobile.tennisbro.R
-
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_list_player.*
 
 class ListPlayerMapActivity : AppCompatActivity() {
 
-    lateinit var mapFragment: PlayerListMapFragment
+    private lateinit var mapFragment: PlayerListMapFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_player)
 
         mapFragment = PlayerListMapFragment.newInstance()
 
+
         supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.map_frame, mapFragment)
                 .commit()
+
+
+        listBtnToolbar.setOnClickListener {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.map_frame, PlayerListFragment())
+                    .commit()
+        }
 
     }
 
